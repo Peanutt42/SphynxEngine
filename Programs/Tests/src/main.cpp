41ae3ae7312cf84a0201b0840f5e2ea3d564f847
Peanutt42 <p3anutt42@gmail.com>
2023-08-11 15:39:19 +0200

    added test github action + headless engine lifecycle test

--- Programs/Tests/src/main.cpp
+++ Programs/Tests/src/main.cpp
@@ -1,6 +1,12 @@
 #include "pch.hpp"
 
 int main(int argc, char** argv) {
+	if (argc >= 1) {
+		std::filesystem::path exePath = std::filesystem::path(argv[0]);
+		if (std::filesystem::exists(exePath))
+			std::filesystem::current_path(exePath.parent_path().parent_path().parent_path().parent_path().parent_path().parent_path());
+	}
+
 	testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
